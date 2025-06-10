d51dada41f11595fc6af6061ff016825146115c4
Idan <sassonidan1@gmail.com>
2025-06-10 18:06:39 +0300

    icons

--- src/components/CategoryModal.jsx
+++ src/components/CategoryModal.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './CategoryModal.css';
 import ReactDOM from 'react-dom'
 import { categories } from './constants';
+import groceries from '../assets/icons/groceries2.png'
 
 export default function CategoryModal( {setIsOpen, setCategory, onClose} ) {
     const handleClick = (cat) => {
@@ -9,13 +10,14 @@ export default function CategoryModal( {setIsOpen, setCategory, onClose} ) {
       setIsOpen(false);
       if (onClose) onClose(cat);
     }
-
+    console.log(groceries);
     return ReactDOM.createPortal(
       <div className='categories-wrapper'>
         <div className='categories-container'>
           {categories.map((cat) => 
           <button key={cat.name} className={`category-button ${cat.name}`} style={{ backgroundColor: cat.color}} onClick={() => handleClick(cat.name)}>
-            {cat.name}
+            <img src={groceries} alt='icon' className='cat-icons'/>
+            {/* {cat.name} */}
           </button>)}
           <div>
             <button onClick={() => setIsOpen(false)}>Close</button>
